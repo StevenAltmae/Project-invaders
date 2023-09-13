1aeef46624f8df4f4141f5b4ccf03e4427aebeb1
St3v3nA <kristo-steven.altmae@ametikool.ee>
2023-09-13 09:31:12 +0300

    small changes

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-    const ship = document.getElementById('ship');
-        const alien = document.getElementById('alien');
-        let alienX = 0;
-        let alienY = 50;
+const ship = document.getElementById('ship');
+const alien = document.getElementById('alien');
+    let alienX = 0;
+    let alienY = 50;
 
-        var leftKey = 37, upKey = 38, rightKey = 39, downKey = 40;
-    var keystate;
-    document.addEventListener("keydown", function (e) {
-        keystate[e.keyCode] = true;
-    });
-    document.addEventListener("keyup", function (e) {
-        delete keystate[e.keyCode];
-    });
+var leftKey = 37, upKey = 38, rightKey = 39, downKey = 40;
+var keystate;
+document.addEventListener("keydown", function (e) {
+    keystate[e.keyCode] = true;
+});
+document.addEventListener("keyup", function (e) {
+    delete keystate[e.keyCode];xx
+});
 
-    if (keystate[leftKey]) {
-        {
-            ship.style.left = parseInt(ship.style.left) - 10 + 'px';
-        }
+if (keystate[leftKey]) {
+    {
+        ship.style.left = parseInt(ship.style.left) - 10 + 'px';
     }
-    if (keystate[upKey]) {
-    //code to be executed when up arrow key is pushed.
+}
+if (keystate[upKey]) {
+}
+if (keystate[rightKey]) {
+    {
+        ship.style.right = parseInt(ship.style.right) + 10 + 'px';
     }
-    if (keystate[rightKey]) {
-        {
-            ship.style.left = parseInt(ship.style.left) + 10 + 'px';
-        }
-    }
-    if (keystate[downKey]) {
-    //code to be executed when down arrow key is pushed.
-    }
+}
+if (keystate[downKey]) {
+}
 
-    function moveAlien() {
-      if (alienX < 280) {
-        alienX += 10;
-      } else {
-        alienX = 0;
-        alienY += 30;
+function moveAlien() {
+    if (alienX < 280) {
+    alienX += 10;
+    } else {
+    alienX = 0;
+    alienY += 30;
       }
       alien.style.left = alienX + 'px';
       alien.style.top = alienY + 'px';
     }
 
-    function checkCollision() {
+function checkCollision() {
       const shipRect = ship.getBoundingClientRect();
       const alienRect = alien.getBoundingClientRect();
 
-      if (
+    if (
         shipRect.right > alienRect.left &&
         shipRect.left < alienRect.right &&
         shipRect.bottom > alienRect.top &&
         shipRect.top < alienRect.bottom
-      ) {
+    ) {
         alert('Game Over!');
         location.reload();
       }
