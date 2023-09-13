2192709d8c270d37eaa38475750b3a265476ba39
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-09-13 09:30:34 +0300

    i h8 n1kke5s

--- script.js
+++ script.js
@@ -1,61 +1,83 @@
-    const ship = document.getElementById('ship');
-        const alien = document.getElementById('alien');
-        let alienX = 0;
-        let alienY = 50;
+const ship = document.getElementById('ship');
+const alien = document.getElementById('alien');
+const bullet = document.getElementById('bullet');
 
-        var leftKey = 37, upKey = 38, rightKey = 39, downKey = 40;
-    var keystate;
-    document.addEventListener("keydown", function (e) {
-        keystate[e.keyCode] = true;
-    });
-    document.addEventListener("keyup", function (e) {
-        delete keystate[e.keyCode];
-    });
+const leftKey = 37;
+const rightKey = 39;
+const spaceKey = 32;
 
-    if (keystate[leftKey]) {
-        {
-            ship.style.left = parseInt(ship.style.left) - 10 + 'px';
+const keystate = {};
+
+document.addEventListener('keydown', (event) => {
+  keystate[event.keyCode] = true;
+});
+
+document.addEventListener('keyup', (event) => {
+  delete keystate[event.keyCode];
+});
+
+function moveShip() {
+  if (keystate[leftKey] && parseInt(getComputedStyle(ship).left) > 0) {
+    ship.style.left = parseInt(getComputedStyle(ship).left) - 10 + 'px';
+  }
+  if (keystate[rightKey] && parseInt(getComputedStyle(ship).left) < window.innerWidth - parseInt(getComputedStyle(ship).width)) {
+    ship.style.left = parseInt(getComputedStyle(ship).left) + 10 + 'px';
+  }
+}
+
+function moveAlien() {
+  const alienPosition = parseInt(getComputedStyle(alien).left);
+  const randomMove = Math.floor(Math.random() * 21) - 10; // Random movement between -10px and 10px
+  const newAlienPosition = alienPosition + randomMove;
+
+  if (newAlienPosition >= 0 && newAlienPosition <= window.innerWidth - parseInt(getComputedStyle(alien).width)) {
+    alien.style.left = newAlienPosition + 'px';
+  }
+}
+
+function shoot() {
+  if (bullet.style.display === 'none') {
+    bullet.style.display = 'block';
+    bullet.style.left = parseInt(getComputedStyle(ship).left) + 20 + 'px';
+    bullet.style.top = (parseInt(getComputedStyle(ship).top) - 10) + 'px';
+    let bulletPosition = parseInt(getComputedStyle(bullet).top);
+    const bulletSpeed = 1;
+
+    function moveBullet() {
+      if (bulletPosition > -10) {
+        bulletPosition -= bulletSpeed;
+        bullet.style.top = bulletPosition + 'px';
+
+        const shipRect = ship.getBoundingClientRect();
+        const alienRect = alien.getBoundingClientRect();
+        const bulletRect = bullet.getBoundingClientRect();
+
+        if (
+          bulletRect.right > alienRect.left &&
+          bulletRect.left < alienRect.right &&
+          bulletRect.bottom > alienRect.top &&
+          bulletRect.top < alienRect.bottom
+        ) {
+          bullet.style.display = 'none';
+          alien.style.left = '50%';
+          alien.style.top = '20px';
+        } else {
+          requestAnimationFrame(moveBullet);
         }
-    }
-    if (keystate[upKey]) {
-    //code to be executed when up arrow key is pushed.
-    }
-    if (keystate[rightKey]) {
-        {
-            ship.style.left = parseInt(ship.style.left) + 10 + 'px';
-        }
-    }
-    if (keystate[downKey]) {
-    //code to be executed when down arrow key is pushed.
-    }
-
-    function moveAlien() {
-      if (alienX < 280) {
-        alienX += 10;
       } else {
-        alienX = 0;
-        alienY += 30;
-      }
-      alien.style.left = alienX + 'px';
-      alien.style.top = alienY + 'px';
-    }
-
-    function checkCollision() {
-      const shipRect = ship.getBoundingClientRect();
-      const alienRect = alien.getBoundingClientRect();
-
-      if (
-        shipRect.right > alienRect.left &&
-        shipRect.left < alienRect.right &&
-        shipRect.bottom > alienRect.top &&
-        shipRect.top < alienRect.bottom
-      ) {
-        alert('Game Over!');
-        location.reload();
+        bullet.style.display = 'none';
       }
     }
 
-    document.addEventListener('keydown', moveShip);
+    moveBullet();
+  }
+}
 
-    setInterval(moveAlien, 1000);
-    setInterval(checkCollision, 100);
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === spaceKey) {
+    shoot();
+  }
+});
+
+setInterval(moveShip, 16);
+setInterval(moveAlien, 1000);
