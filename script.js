2b98222781ddda7b62b463278e5c540191391ff8
St3v3nA <kristo-steven.altmae@ametikool.ee>
2023-09-15 09:53:09 +0300

    Update

--- script.js
+++ script.js
@@ -12,48 +12,70 @@ document.addEventListener("keyup", function (e) {
     delete keystate[e.keyCode];xx
 });
 
-if (keystate[leftKey]) {
-    {
-        ship.style.left = parseInt(ship.style.left) - 10 + 'px';
-    }
-}
-if (keystate[upKey]) {
-}
-if (keystate[rightKey]) {
-    {
-        ship.style.right = parseInt(ship.style.right) + 10 + 'px';
-    }
-}
-if (keystate[downKey]) {
+document.addEventListener('keyup', (event) => {
+  delete keystate[event.keyCode];
+});
+
+function moveShip() {
+  if (keystate[leftKey] && parseInt(getComputedStyle(ship).left) > 40) {
+    ship.style.left = parseInt(getComputedStyle(ship).left) - 10 + 'px';
+  }
+  if (keystate[rightKey] && parseInt(getComputedStyle(ship).left) < window.innerWidth - parseInt(getComputedStyle(ship).width)) {
+    ship.style.left = parseInt(getComputedStyle(ship).left) + 10 + 'px';
+  }
 }
 
 function moveAlien() {
-    if (alienX < 280) {
-    alienX += 10;
-    } else {
-    alienX = 0;
-    alienY += 30;
-      }
-      alien.style.left = alienX + 'px';
-      alien.style.top = alienY + 'px';
-    }
+  const alienPosition = parseInt(getComputedStyle(alien).left);
+  const randomMove = Math.floor(Math.random() * 21) - 10;
+  const newAlienPosition = alienPosition + randomMove;
 
 function checkCollision() {
       const shipRect = ship.getBoundingClientRect();
       const alienRect = alien.getBoundingClientRect();
 
-    if (
-        shipRect.right > alienRect.left &&
-        shipRect.left < alienRect.right &&
-        shipRect.bottom > alienRect.top &&
-        shipRect.top < alienRect.bottom
-    ) {
-        alert('Game Over!');
-        location.reload();
+function shoot() {
+  if (bullet.style.display === 'none') {
+    bullet.style.display = 'block';
+    bullet.style.left = parseInt(getComputedStyle(ship).left) + 20 + 'px';
+    bullet.style.top = (parseInt(getComputedStyle(ship).top) - 10) + 'px';
+    let bulletPosition = parseInt(getComputedStyle(bullet).top);
+    const bulletSpeed = 1;
+  }
+    function moveBullet() {
+      if (bulletPosition > -10) {
+        bulletPosition -= bulletSpeed;
+        bullet.style.top = bulletPosition + 'px';
+
+        const shipRect = ship.getBoundingClientRect();
+        const alienRect = alien.getBoundingClientRect();
+        const bulletRect = bullet.getBoundingClientRect();
+
+        if (
+          bulletRect.right > alienRect.left &&
+          bulletRect.left < alienRect.right &&
+          bulletRect.bottom > alienRect.top &&
+          bulletRect.top < alienRect.bottom
+        ) {
+          bullet.style.display = 'none';
+          alien.style.left = '50%';
+          alien.style.top = '20px';
+        } else {
+          requestAnimationFrame(moveBullet);
+        }
+      } else {
+        bullet.style.display = 'none';
       }
-    }
+      moveBullet();
+  }
+}
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === spaceKey) {
+    shoot();
+  }
+});
 
-    document.addEventListener('keydown', moveShip);
-
-    setInterval(moveAlien, 1000);
-    setInterval(checkCollision, 100);
+  setInterval(moveShip, 16);
+  setInterval(moveAlien, 1000);
+  }
+}
