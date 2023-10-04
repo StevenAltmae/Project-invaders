14bc825b4fca1e92bccedec6fe1a84e8fca9553a
St3v3nA <kristo-steven.altmae@ametikool.ee>
2023-10-04 19:41:00 +0300

    working on start and restart screen

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ canvas.width = 1000;
 canvas.height = 600;
 
 const background = new Image();
-background.src = "images/space.jpg";
+background.src = "space.jpg";
 
 const playerBulletController = new BulletController(canvas, 10, "red", true);
 const enemyBulletController = new BulletController(canvas, 4, "pink", false);
@@ -22,8 +22,14 @@ const player = new Player(canvas, 3, playerBulletController);
 
 let isGameOver = false;
 let didWin = false;
+let isGameStarted = false; 
 
-function game() {
+function startGame() {
+    isGameStarted = true;
+    requestAnimationFrame(gameLoop); 
+  }
+
+function gameLoop() {
   checkGameOver();
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   displayGameOver();
@@ -32,8 +38,46 @@ function game() {
     player.draw(ctx);
     playerBulletController.draw(ctx);
     enemyBulletController.draw(ctx);
+    requestAnimationFrame(gameLoop); 
   }
 }
+
+function displayStartMenu() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "white";
+    ctx.font = "36px Arial";
+    ctx.fillText("Space Invaders", canvas.width / 2 - 100, canvas.height / 2 - 50);
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(canvas.width / 2 - 60, canvas.height / 2, 150, 40);
+    ctx.fillStyle = "white";
+    ctx.font = "24px Arial";
+    ctx.fillText("Start Game", canvas.width / 2 - 45, canvas.height / 2 + 30);
+
+    canvas.addEventListener("click", handleStartClick);
+  }
+
+  function handleStartClick(event) {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    if (
+        mouseX >= canvas.width / 2 - 60 &&
+        mouseX <= canvas.width / 2 + 60 &&
+        mouseY >= canvas.height / 2 &&
+        mouseY <= canvas.height / 2 + 40
+      ) {
+
+        canvas.removeEventListener("click", handleStartClick);
+        startGame();
+      }
+    }
+
+
+displayStartMenu();
 
 function displayGameOver() {
   if (isGameOver) {
@@ -50,15 +94,12 @@ function checkGameOver() {
   if (isGameOver) {
     return;
   }
-
   if (enemyBulletController.collideWith(player)) {
     isGameOver = true;
   }
-
   if (enemyController.collideWith(player)) {
     isGameOver = true;
   }
-
   if (enemyController.enemyRows.length === 0) {
     didWin = true;
     isGameOver = true;
